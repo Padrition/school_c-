29ba5d335154e18ee5a33818b6dbc5e7c4deb7ec
Padrition <padrition@gmail.com>
2022-11-15 17:58:48 +0100

    Implemented method setPlayerCoords; Player constructor now utilizes setPlayerCoords method

--- Player/Player.cpp
+++ Player/Player.cpp
@@ -1,7 +1,18 @@
 #include "Player.h"
 
 Player::Player(int x, int y){
+    setPlayerCoords(x, y);
+}
+
+Coords Player::getPlayerCoords(){
+    return coords;
+}
+
+void Player::setPlayerCoords(int x, int y){
     coords.x = x;
     coords.y = y;
+}
 
+void Player::parseEvent(){
+    //TODO
 }
