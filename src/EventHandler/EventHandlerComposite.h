1e7dd779b3611f8dca412922e54713f839c92af9
Padrition <padrition@gmail.com>
2022-11-19 00:37:46 +0100

    Changed vector signature from EventHandler to EventHnadlerLeaf; added currentEvent field

--- src/EventHandler/EventHandlerComposite.h
+++ src/EventHandler/EventHandlerComposite.h
@@ -2,13 +2,17 @@
 #define EVENTHANDLERCOMPOSITE_H
 #include "EventHandler.h"
 #include <vector>
+#include <component.hpp>
+#include "EventHandlerLeaf.h"
 class EventHandlerComposite : protected EventHandler{
     private:
-    std::vector<EventHandler *> eventChildren;
+    std::vector<EventHandlerLeaf *> eventChildren;
+    ftxui::Event currentEvent;
 
     public:
     //In this constructor we want to add all Events to eventChildren_ vector
     EventHandlerComposite();
+    void listen();
     void handle();
 };
 #endif
