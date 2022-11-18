3604aa8b621a87eefd79f37a1938323f7a06c69b
Padrition <padrition@gmail.com>
2022-11-19 00:38:38 +0100

    Implemented listen() and handle() methods

--- src/EventHandler/EventHandlerComposite.cpp
+++ src/EventHandler/EventHandlerComposite.cpp
@@ -1,5 +1,5 @@
 #include "EventHandlerComposite.h"
-
+using namespace ftxui;
 //In the constructor we want to add all EventChildren to eventChildren_ vector
 EventHandlerComposite::EventHandlerComposite(){
     
@@ -8,5 +8,16 @@ EventHandlerComposite::EventHandlerComposite(){
 //this method will derevate all the task to its children by calling handle() method from
 //The needed child 
 void EventHandlerComposite::handle(){
-    
+    for(EventHandlerLeaf * child : eventChildren){
+        if(child->getUniqueEvent() == currentEvent){
+            child->handle();
+        }
+    }
+}
+
+void EventHandlerComposite::listen(){
+    CatchEvent([&](Event event){
+        currentEvent = event;
+        return true;
+    });
 }
