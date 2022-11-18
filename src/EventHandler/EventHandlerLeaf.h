799ca573aa7b536885cf677ec8039d269cbdbfcd
Padrition <padrition@gmail.com>
2022-11-19 00:09:18 +0100

    Created abstract class EventHandelrLeaf for evnts

---
+++ src/EventHandler/EventHandlerLeaf.h
@@ -0,0 +1,15 @@
+#ifndef EVENTHANDLERLEAF_H
+#define EVENTHANDLERLEAF_H
+#include <EventHandler.h>
+#include <component.hpp>
+class EventHandlerLeaf : protected EventHandler{
+    private:
+    ftxui::Event uniqueEvent;
+
+    public:
+    ftxui::Event getUniqueEvent(){
+        return uniqueEvent;
+    };
+    virtual void handle();
+};
+#endif
