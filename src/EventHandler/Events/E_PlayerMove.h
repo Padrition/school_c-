4c00777cfbc0f648276eb90798fd15188a56869a
Padrition <padrition@gmail.com>
2022-12-05 21:34:45 +0100

    Init pointer to Character in PlayerMove class

--- src/EventHandler/Events/E_PlayerMove.h
+++ src/EventHandler/Events/E_PlayerMove.h
@@ -1,10 +1,14 @@
 #ifndef E_PLAYERMOVE_H
 #define E_PLAYERMOVE_H
+
+#include "../Character/Character.h"
+
 class E_PlayerMove{
     protected:
     const char M_RIGHT = 'd';
     const char M_LEFT = 'a';
     const char M_UP = 'w';
     const char M_DOWN = 's';
+    Character * player;
 };
 #endif
