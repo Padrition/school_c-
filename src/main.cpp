b31e6a0f49d6a8739ddeab41a36683a28119141d
Padrition <padrition@gmail.com>
2022-12-11 18:32:24 +0100

    12

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,28 @@
+#include "Scene/Scene.h"
+#include "Player/Player.h"
+#include "EventHandler/EventHandlerComposite.h"
+#include "Mediator/BaseMediator.h"
+#include <iostream>
+#include "ftxui/component/screen_interactive.hpp"
+#include "Map/Map.h"
+using namespace ftxui;
 int main(){
+    auto screen = ScreenInteractive::TerminalOutput();
     
-    return 0;
+    Scene * scene = new Scene();
+    Player * player = new Player(1,1);
+    EventHandlerComposite * evc = new EventHandlerComposite();
+
+    BaseMediator * mediator = new BaseMediator(scene, player, evc);
+    
+    
+
+    auto component = Renderer([&] {
+        Elements children;
+        return window(text("keys"),vbox(text(player->coordsToString())));
+    });
+    
+    evc->listen(component);
+
+    screen.Loop(component);
 }
